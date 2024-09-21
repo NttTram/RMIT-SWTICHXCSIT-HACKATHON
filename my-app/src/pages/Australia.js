e4918dfe3ef7897c09538ce272fa2e19da6422dd
TommyNgucode <s4093392@student.rmit.edu.au>
2024-09-21 17:20:59 +1000

    Contact page info, Australian page info

--- my-app/src/pages/Australia.js
+++ my-app/src/pages/Australia.js
@@ -8,7 +8,23 @@ const Australia = () => {
           <h2>Welcome to Australia Page</h2>
           <p>This is the Australia page content.</p>
 
-          {/* just have information for help ease the culture shocks, slang, manners, etc */}
+          {
+          <p> <a href="https://dictionary.cambridge.org/dictionary/english/g-day">G'day</a> <a>mate</a>, we’re thrilled to have you here as you embark on this exciting journey! As you settle
+            in, remember that it's perfectly normal to be feel anxious and confused.
+           
+            Now, we're going to teach you some <a href="https://www.ingeniaholidays.com.au/blog/dictionary-aussie-words-ph
+            rases/#:~:text=Ripper%20%E2%80%93%20really%20great%2C%20as%20in,a%20ripper%20time%20on%20holidays'.">ripper</a> Australian slang! One of the most noticeable ones is “<a href="https://dictionary.cambridge.org/dictionary/english/g-day">G'day</a>,” which means “Good day.” It’s a friendly way to say hello.<br /><br/>
+            Let’s dive into some more <a href="https://www.ingeniaholidays.com.au/blog/dictionary-aussie-words-ph
+            rases/#:~:text=Ripper%20%E2%80%93%20really%20great%2C%20as%20in,a%20ripper%20time%20on%20holidays'.">ripper</a> phrases:<br /><br />
+
+            As you might of realised by now, <a href="https://www.ingeniaholidays.com.au/blog/dictionary-aussie-words-ph
+            rases/#:~:text=Ripper%20%E2%80%93%20really%20great%2C%20as%20in,a%20ripper%20time%20on%20holidays'.">ripper</a> acts as a adjective and refers to something fantastic. You might say, “That was a ripper of a game last night!”
+            Lastly, “Mate” is a classic term for a friend or buddy. You might say, “How’s it going, mate?”<br /><br />
+
+            Explore the vibrant food scenes in cities like <a href="https://en.wikipedia.org/wiki/Melbourne">Melbourne</a> and <a href="https://en.wikipedia.org/wiki/Sydney">Sydney</a>, featuring everything from authentic Asian cuisines to trendy brunch spots. 
+            Embrace the unique flavors of Australia and create delicious memories!
+             </p>
+          /* just have information for help ease the culture shocks, slang, manners, etc */}
 
 
 
